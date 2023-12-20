c4e6e1a3e1e9abd275ade1b2968119d04af0addd
yomisana <sammywhy3236@gmail.com>
2023-12-20 22:56:36 +0800

    可以正常運作囉!

--- app.js
+++ app.js
@@ -2,27 +2,43 @@
 const linebot = require('linebot');
 require('dotenv').config();
 
+const axios = require('axios');
+
 const bot = linebot({
     channelId: process.env.CHANNEL_ID,
     channelSecret: process.env.CHANNEL_SECRET,
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
+
 // 當有人傳送訊息給Bot時
 bot.on('message', function (event) {
-    // // event.message.text是使用者傳給bot的訊息
-    // // 準備要回傳的內容
-    console.log(event.message.text);
-    // const replyMsg = `Hello你剛才說的是:${event.message.text}`;
-    // // 透過event.reply(要回傳的訊息)方法將訊息回傳給使用者
-    // event.reply(replyMsg).then(function (data) {
-    //     // 當訊息成功回傳後的處理
-    // }).catch(function (error) {
-    //     // 當訊息回傳失敗後的處理
-    // });
+    // event.message.text是使用者傳給bot的訊息
+    // 準備要回傳的內容
+    // console.log(event.message.text);
+    const options = {
+        method: "post",
+        url: "https://notify-api.line.me/api/notify",
+        headers: {
+            "Authorization": `Bearer ${process.env.LINE_NOTIFY_TOKEN}`,
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data: `message=\n${event.message.text}`,
+    };
+
+    axios(options)
+        .then(response => {
+            console.log(response.data);
+            res.send('Line Notify sent successfully!');
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send('Error sending Line Notify');
+        });
 });
 
 // Bot所監聽的webhook路徑與port
 bot.listen(`/${process.env.URL_ROOT}`, 3210, function () {
     console.log('[BOT已準備就緒]');
     console.log(`URL:https://${process.env.DOMAIN_NAME}/${process.env.URL_ROOT}`)
+    console.log('[BOT已準備就緒]');
 });
